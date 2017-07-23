57c10e5d3f85d9a787778ca07529889769029533
Ajai <ajainarayanan@gmail.com>
2017-07-22 22:25:35 -0700

    Pretifier linting changes

--- components/NodesList/NodesList.js
+++ components/NodesList/NodesList.js
@@ -9,50 +9,49 @@ type propType = {
   renderNode: (x: Object) => ?React$Element<any>,
   onNodesClick: (x: Object) => void,
   nodes: Array<Object>,
-  jsPlumbInstance: Object
-}
+  jsPlumbInstance: Object,
+};
 export default class NodesList extends Component {
   state: Object;
   constructor(props: propType) {
     super(props);
-    let { nodes = []} = props;
+    let { nodes = [] } = props;
     this.state = {
-      nodes: nodes.map( node => Object.assign({}, node, { id: node.id || `N-${shortid.generate()}` }) )
+      nodes: nodes.map(node =>
+        Object.assign({}, node, { id: node.id || `N-${shortid.generate()}` }),
+      ),
     };
   }
   componentWillReceiveProps(newProps: propType) {
     this.setState({
-      nodes: newProps.nodes.map(
-        node => {
-          if (node.id) {
-            return node;
-          }
-          return Object.assign({}, node, { id: node.id || `N-${shortid.generate()}` })
+      nodes: newProps.nodes.map(node => {
+        if (node.id) {
+          return node;
         }
-      )
+        return Object.assign({}, node, {
+          id: node.id || `N-${shortid.generate()}`,
+        });
+      }),
     });
   }
   render() {
     return (
       <div className="dag-nodes-list">
-        {
-          this.state
-            .nodes
-            .map( node => {
-              if (this.props.renderNode) {
-                return this.props.renderNode(node);
-              }
-              return (
-                <Node style={node.style}
-                  type={node.type}
-                  label={node.label}
-                  key={node.id}
-                  id={node.id}
-                  onNodesClick={this.props.onNodesClick}
-                />
-              )
-            })
-        }
+        {this.state.nodes.map(node => {
+          if (this.props.renderNode) {
+            return this.props.renderNode(node);
+          }
+          return (
+            <Node
+              style={node.style}
+              type={node.type}
+              label={node.label}
+              key={node.id}
+              id={node.id}
+              onNodesClick={this.props.onNodesClick}
+            />
+          );
+        })}
       </div>
     );
   }
